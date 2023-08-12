7f730a9939b33ebf3616fede89c888d4c883037e
juanfra312003 <80067016+juanfra312003@users.noreply.github.com>
2023-08-12 09:08:27 -0500

    feat-countries-load-list-view

--- app/src/main/java/com/example/taller_uno/FrameActivity.kt
+++ app/src/main/java/com/example/taller_uno/FrameActivity.kt
@@ -2,10 +2,16 @@ package com.example.taller_uno
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.taller_uno.databinding.ActivityFrameBinding
 
 class FrameActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityFrameBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_frame)
+        binding = ActivityFrameBinding.inflate(layoutInflater)
+        val greeting = intent.getStringExtra("greeting")
+        binding.greetingText.text = greeting
+        setContentView(binding.root)
     }
 }
--- app/src/main/java/com/example/taller_uno/MainActivity.kt
+++ app/src/main/java/com/example/taller_uno/MainActivity.kt
@@ -4,6 +4,8 @@ import android.content.Intent
 import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.Toast
 import com.example.taller_uno.databinding.ActivityMainBinding
 import com.example.taller_uno.databinding.ActivityTicTacToeBinding
 
@@ -14,11 +16,39 @@ class MainActivity : AppCompatActivity() {
         var intent = Intent (baseContext, TicTacToeActivity::class.java)
         startActivity(intent)
     }
+
+    fun buttonCountriesAction(){
+        var intent = Intent (baseContext, ListViewActivity::class.java)
+        startActivity(intent)
+    }
+
+    fun buttonFrameActivity(itemSelected : String){
+        if (itemSelected.isBlank() || itemSelected == " "){
+            Toast.makeText(baseContext, "You must to select one language.", Toast.LENGTH_LONG).show()
+        }
+        else{
+            var intent = Intent (baseContext, FrameActivity::class.java)
+            val greeting : String = when (itemSelected){
+                "Español" -> "Hola."
+                "English" -> "Hello"
+                "Français"->"Salut"
+                "Italiano"->"Ciao"
+                "Português"->"Olá"
+                else -> ""
+            }
+            println(greeting)
+            intent.putExtra("greeting", greeting)
+            startActivity(intent)
+        }
+
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
         binding.buttonTicTacToe.setOnClickListener{buttonTicTacToeAction()}
+        //val adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, )
+        binding.buttonCountries.setOnClickListener{buttonCountriesAction()}
     }
 }
